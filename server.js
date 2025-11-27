fa3a24bd598e4dd76e6e62a4093161f9df207adc
Diamondszone <120200944+Diamondszone@users.noreply.github.com>
2025-11-28 04:42:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const cors_proxy = require('cors-anywhere');
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
+
+cors_proxy.createServer({
+    originWhitelist: [], // allow all
+    requireHeader: [],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
